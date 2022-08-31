298837745b08c05a86c35404557512604c72f096
starstormfall <photon_dazzles_02@icloud.com>
2022-08-31 19:07:12 +0800

    completed up to comfortable wihout backend filter yet

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+
+const cors = require("cors");
+
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings?filter=");
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  res.json(sightings[req.params.sightingIndex]);
 });
 
 app.listen(PORT, () => {
